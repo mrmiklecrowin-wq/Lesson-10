5acde7399f1e31c353247a9d6d415acc54840e65
mrmiklecrowin-wq <mr.miklecrowin@gmail.com>
2026-06-17 15:16:32 +0300

    Update Lesson_10_2.cpp

--- Lesson_10_2.cpp
+++ Lesson_10_2.cpp
@@ -1,18 +1,14 @@
 #include<iostream>
 #include<windows.h>
 
-double create_array(int& size_arr){
-	std::cout<<"Массив: ";
-	int i = 0;
+double create_array(int &size_arr){	
 	double* arr = new double[size_arr]();
-	while(i < size_arr){
-	std::cout<<(double)arr[i]<< " ";
-	i++;	
+	for(int i = 0;i < size_arr; i++){
+	std::cout<<(double)arr[i]<< " ";	
 	}
 	delete[]arr;
-	return i;
+	return *arr;
 }
-
 
 
 int main(){
@@ -20,11 +16,12 @@ int main(){
 	SetConsoleOutputCP(1251);
 	
 	int size_arr = 0;
-	
+
 	std::cout<<"Введите размер массива: ";
 	std::cin>>size_arr;
 	
+	std::cout<<"Массив: ";
 	create_array(size_arr);
-	
+
 return 0;	
 }
